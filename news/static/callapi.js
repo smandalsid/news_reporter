d9ef51d220412c3540f9e907e6817d1e4248b6d5
smandalsid <smandal.sid@gmail.com>
2023-06-09 10:54:44 +0530

    final

--- news/static/callapi.js
+++ news/static/callapi.js
@@ -1,135 +1,53 @@
-var selected="all"
-
+var selected="all";
 
 $(document).ready(function(){
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            
-            // l=JSON.parse(this.responseText);
-            // document.getElementById("tablebod").innerHTML=l
-            var json=JSON.parse(this.responseText)
-            for(var i=0;i<json.length;i++)
-            {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
-            }
-        }
-    };
-    xmlhttp.open("GET", "http://127.0.0.1:8000/api/google/", true);
-    xmlhttp.send();
-    
-    var xmlhttp2=new XMLHttpRequest();
-    xmlhttp2.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            
-            // l=JSON.parse(this.responseText);
-            // document.getElementById("tablebod").innerHTML=l
-            var json=JSON.parse(this.responseText)
-            for(var i=0;i<json.length;i++)
-            {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
-            }
-        }
-    };
-    xmlhttp2.open("GET", "http://127.0.0.1:8000/api/apple/", true);
-    xmlhttp2.send();
-
-    var xmlhttp3=new XMLHttpRequest();
-    xmlhttp3.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            
-            // l=JSON.parse(this.responseText);
-            // document.getElementById("tablebod").innerHTML=l
-            var json=JSON.parse(this.responseText)
-            for(var i=0;i<json.length;i++)
-            {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
-            }
-        }
-    };
-    xmlhttp3.open("GET", "http://127.0.0.1:8000/api/microsoft/", true);
-    xmlhttp3.send();
-
-    var xmlhttp4=new XMLHttpRequest();
-    xmlhttp4.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            
-            // l=JSON.parse(this.responseText);
-            // document.getElementById("tablebod").innerHTML=l
-            var json=JSON.parse(this.responseText)
-            for(var i=0;i<json.length;i++)
-            {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
-            }
-        }
-    };
-    xmlhttp4.open("GET", "http://127.0.0.1:8000/api/meta/", true);
-    xmlhttp4.send();
+    allnews();
 });
 
 function allnews(){
-    selected="all"
+    selected="all";
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
         if(this.readyState==4 && this.status==200){
             
-            $("#tablebod").html("");
-            // l=JSON.parse(this.responseText);
-            // document.getElementById("tablebod").innerHTML=l
             var json=JSON.parse(this.responseText)
+            $("#tablebod").html("");
             for(var i=0;i<json.length;i++)
             {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
+                if(document.getElementById("from").value && document.getElementById("to").value)
+                {
+                    var date1=new Date(document.getElementById("from").value);
+                    var date2=new Date(document.getElementById("to").value);
+                    var temp= new Date(json[i].ago);
+                    if(temp>=date1 && temp<=date2)
+                    {
+                        $("#tablebod").append("<tr>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].query);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].title);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].ago);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("</tr>");
+                    }
+                }
+                else
+                {
+                    $("#tablebod").append("<tr>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].query);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].title);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].ago);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("</tr>");
+                }
             }
         }
     };
@@ -145,17 +63,41 @@ function allnews(){
             var json=JSON.parse(this.responseText)
             for(var i=0;i<json.length;i++)
             {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
+                if(document.getElementById("from").value && document.getElementById("to").value)
+                {
+                    var date1=new Date(document.getElementById("from").value);
+                    var date2=new Date(document.getElementById("to").value);
+                    var temp= new Date(json[i].ago);
+                    if(temp>=date1 && temp<=date2)
+                    {
+                        $("#tablebod").append("<tr>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].query);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].title);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].ago);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("</tr>");
+                    }
+                }
+                else
+                {
+                    // alert("LOL");
+                    $("#tablebod").append("<tr>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].query);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].title);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].ago);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("</tr>");
+                }
             }
         }
     };
@@ -171,17 +113,41 @@ function allnews(){
             var json=JSON.parse(this.responseText)
             for(var i=0;i<json.length;i++)
             {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
+                if(document.getElementById("from").value && document.getElementById("to").value)
+                {
+                    var date1=new Date(document.getElementById("from").value);
+                    var date2=new Date(document.getElementById("to").value);
+                    var temp= new Date(json[i].ago);
+                    if(temp>=date1 && temp<=date2)
+                    {
+                        $("#tablebod").append("<tr>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].query);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].title);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].ago);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("</tr>");
+                    }
+                }
+                else
+                {
+                    // alert("LOL");
+                    $("#tablebod").append("<tr>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].query);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].title);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].ago);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("</tr>");
+                }
             }
         }
     };
@@ -197,17 +163,41 @@ function allnews(){
             var json=JSON.parse(this.responseText)
             for(var i=0;i<json.length;i++)
             {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
+                if(document.getElementById("from").value && document.getElementById("to").value)
+                {
+                    var date1=new Date(document.getElementById("from").value);
+                    var date2=new Date(document.getElementById("to").value);
+                    var temp= new Date(json[i].ago);
+                    if(temp>=date1 && temp<=date2)
+                    {
+                        $("#tablebod").append("<tr>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].query);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].title);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].ago);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("</tr>");
+                    }
+                }
+                else
+                {
+                    // alert("LOL");
+                    $("#tablebod").append("<tr>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].query);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].title);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].ago);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("</tr>");
+                }
             }
         }
     };
@@ -217,7 +207,7 @@ function allnews(){
 
 
 function getnews(comp) {
-    selected=comp
+    selected=comp;
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
         if(this.readyState==4 && this.status==200){
@@ -225,30 +215,77 @@ function getnews(comp) {
             // l=JSON.parse(this.responseText);
             // document.getElementById("tablebod").innerHTML=l
             var json=JSON.parse(this.responseText)
-            $("#tablebod").html("<tr>");
+            $("#tablebod").html("");
             for(var i=0;i<json.length;i++)
             {
-                $("#tablebod").append("<tr>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].query);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].title);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("<td>");
-                $("#tablebod").append(json[i].ago);
-                $("#tablebod").append("</td>");
-                $("#tablebod").append("</tr>");
+                if(document.getElementById("from").value && document.getElementById("to").value)
+                {
+                    var date1=new Date(document.getElementById("from").value);
+                    var date2=new Date(document.getElementById("to").value);
+                    var temp= new Date(json[i].ago);
+                    if(temp>=date1 && temp<=date2)
+                    {
+                        $("#tablebod").append("<tr>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].query);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].title);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("<td>");
+                        $("#tablebod").append(json[i].ago);
+                        $("#tablebod").append("</td>");
+                        $("#tablebod").append("</tr>");
+                    }
+                }
+                else
+                {
+                    // alert("LOL");
+                    $("#tablebod").append("<tr>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].query);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].title);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("<td>");
+                    $("#tablebod").append(json[i].ago);
+                    $("#tablebod").append("</td>");
+                    $("#tablebod").append("</tr>");
+                }
             }
         }
+        // document.getElementById("from").value="";
+        // document.getElementById("to").value="";
     };
     xmlhttp.open("GET", "http://127.0.0.1:8000/api/"+comp+"/", true);
     xmlhttp.send();
 }
 
 function filter() {
+    // alert("LOL");
     date1=document.getElementById("from").value;
     date2=document.getElementById("to").value;
-    alert(date1);
-    alert(date2);
+    if(date1 && date2)
+    {
+        if(selected=="all")
+        {
+            allnews();
+        }
+        else
+        {
+            getnews(selected);
+        }
+    }
+    else if(!date1 && !date2)
+    {
+        if(selected=="all")
+        {
+            allnews();
+        }
+        else
+        {
+            getnews(selected);
+        }
+    }
 }
